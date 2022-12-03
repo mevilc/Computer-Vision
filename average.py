b5975b54b6876f734a89abd68ebc00e0cae0f976
Mevil Crasta <39920489+mevilc@users.noreply.github.com>
2022-12-03 01:30:06 -0500

    Delete average.py

--- average.py
+++
@@ -1,9 +0,0 @@
-import cv2, numpy as np
-
-# Averaging is performed in the neighborhood to fill the gaps (zeroes)
-def averaging(left, right):
-    K_shape = (5, 5)
-    K = np.ones(K_shape, np.float32) / 5
-    left = cv2.filter2D(left, -1, K)
-    right = cv2.filter2D(right, -1, K)
-    return left, right
